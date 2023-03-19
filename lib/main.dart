c2a05c341ee5ac1e3294d73afda1aae3907b808b
habyaad <adeomotoshan@gmail.com>
2023-03-19 07:51:19 +0100

    refactor: refactor main.dart with router class

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
-import 'package:ephiway/ui/features/onboarding/onboarding_screen.dart';
+import 'package:ephiway/utils/app_router/app_router.gr.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final _appRouter = AppRouter();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
       title: 'Flutter Demo',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home:  OnboardingScreen(),
     );
   }
 }
