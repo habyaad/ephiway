073b5922e94b5c98965e3398ba79de670b367d4c
habyaad <adeomotoshan@gmail.com>
2023-03-26 07:40:43 +0100

    refactor: implement singleton for app router

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:ephiway/utils/app_router/app_router.gr.dart';
 import 'package:ephiway/utils/helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   setupLocator();
-  runApp( MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-  final _appRouter = AppRouter();
+
+  final _appRouter = getIt<AppRouter>();
 
   // This widget is the root of your application.
   @override
