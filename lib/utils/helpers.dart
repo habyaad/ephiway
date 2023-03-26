142c7a77ac3661cfa48ccaf176d22f6789ef8f65
habyaad <adeomotoshan@gmail.com>
2023-03-26 07:40:23 +0100

    refactor: register singleton for app router

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -1,10 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
 
+import 'app_router/app_router.gr.dart';
+
 final getIt = GetIt.instance;
 
 Future<void> setupLocator() async {
-  getIt.registerLazySingleton(() => Logger());
+  getIt.registerLazySingleton<Logger>(() => Logger());
+  getIt.registerLazySingleton<AppRouter>(() => AppRouter());
 
 // Alternatively you could write it if you don't like global variables
   // GetIt.I.registerSingleton<AppModel>(AppModel());
